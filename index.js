53cf1616a93c789f0f6f6c2e1c76138488863978
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2022-11-03 00:30:41 -0600

    Required the new Review model. Set up Create and Delete routes for review. Set up logic for removing ONE review out of the array of reviews

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 const path = require('path')
 const mongoose = require('mongoose');
 const engine = require('ejs-mate'); // one of many engines used to run/parse ejs. this is the one we want to use rather than the default one
+const methodOverride = require('method-override')
+
 const catchAsync = require('./utils/catchAsync')
 const Campground = require('./models/campground')
-const methodOverride = require('method-override')
 const ExpressError = require('./utils/ExpresError')
-const { campgroundSchema } = require('./schemas')
+const { campgroundSchema, reviewSchema } = require('./schemas')
+const Review = require('./models/review')
+
 
 
 main().catch(err => console.log(`oh no mongo ${err}`));
@@ -36,6 +39,15 @@ const validateCampground = (req, res, next) => {
     }
 }
 
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -58,18 +70,17 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 
 
-
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params
-    const campground = await Campground.findById(req.params.id);
-    const url = campground.imageUrl;
-    res.render('./campgrounds/show', { campground, url })
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    res.render('./campgrounds/show', { campground })
 }))
+
+
+
 
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id);
-    console.log(campground.location)
     res.render(`./campgrounds/edit`, { campground })
 }))
 
@@ -83,6 +94,21 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
+}))
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review)
+    campground.reviews.push(review)
+    await Promise.all([review.save(), campground.save()])  // awaiting two things at once
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }) //first find the campground with the ID, then pass in an object that uses the $pull mongoose operator to pull from an existing array every value taht matches my condition, in this case the condition is having the specific reviewID from the button i'm pressing
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/campgrounds/${id}`)
 }))
 
 app.all('*', (req, res, next) => {
